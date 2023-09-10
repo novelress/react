4814192ca2d2741e55a8622ab0fc3dc10ccb6951
novelress <tagloose1337@gmail.com>
2023-09-10 13:39:58 +0300

    movie

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AUTH_TOKEN } from "../../helpers/helpers";
 import "../Home/Home.css";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Typography } from "@mui/material";
 import MovieCard from "../../widgets/MovieCard/MovieCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
   const [movieslist, setMoviesList] = useState(null);
   const [genres, setGenres] = useState({});
-  const [showAll, setShowAll] = useState(false);
   const [visibleMovies, setVisibleMovies] = useState([]);
+  const sliderRef = useRef(null);
+  const totalMovies = 15;
+  const moviesPerPage = 5;
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     const options = {
@@ -40,7 +46,7 @@ const Home = () => {
       .then((response) => response.json())
       .then((response) => {
         setMoviesList(response.results);
-        setVisibleMovies(response.results.slice(0, 5));
+        setVisibleMovies(response.results.slice(0, moviesPerPage));
       })
       .catch((err) => console.error(err));
   }, []);
@@ -48,37 +54,123 @@ const Home = () => {
   if (!movieslist) {
     return <p>Loading...</p>;
   }
-  console.log(movieslist);
 
-  const toggleShowAll = () => {
-    setShowAll(!showAll);
-    if (showAll) {
-      setVisibleMovies(movieslist.slice(0, 8));
-    } else {
-      setVisibleMovies(movieslist);
-    }
-    
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: moviesPerPage,
+    slidesToScroll: moviesPerPage,
   };
+
+  const nextSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+      setCurrentPage(currentPage + 1);
+      const startIndex = (currentPage + 1) * moviesPerPage;
+      const endIndex = startIndex + moviesPerPage;
+      setVisibleMovies(movieslist.slice(startIndex, endIndex));
+    }
+  };
+
+  const prevSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+      setCurrentPage(currentPage - 1);
+      const startIndex = (currentPage - 1) * moviesPerPage;
+      const endIndex = startIndex + moviesPerPage;
+      setVisibleMovies(movieslist.slice(startIndex, endIndex));
+    }
+  };
+
+  const containerStyle = {
+    maxWidth: "780px",
+    margin: "0 auto",
+  };
+
+  const textStyle = {
+    textAlign: "left",
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <p>Home Page</p>
-      <h1>Список популярных фильмов</h1>
-      <div className="movie-list">
-        {visibleMovies.map((movie, index) => (
-          <MovieCard movie={movie} key={index} genres={genres} voteAverage={movie.vote_average} />
-        ))}
+      <div>
+      <div style={{ display: "flex", justifyContent: "space-between", paddingRight: '5px' }}>
+  <div style={{ ...textStyle, marginRight: "0px", flex: "0 0 50%" }}>Best Rating</div>
+  <div style={{ display: "flex", alignItems: "center" }}>
+    <button
+      onClick={prevSlide}
+      disabled={currentPage === 0}
+      style={{
+        width: "25px",
+        height: "20px",
+        backgroundColor: "white",
+        textAlign: "center",
+        alignItems: "center",
+        border: "1px solid gray",
+        cursor: "pointer",
+        padding: "0",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{ verticalAlign: "middle" }}
+      >
+        <path d="M15 18l-6-6 6-6" />
+      </svg>
+    </button>
+    <button
+      onClick={nextSlide}
+      disabled={currentPage === totalMovies / moviesPerPage - 1}
+      style={{
+        marginLeft: '5px',
+        width: "25px",
+        height: "20px",
+        backgroundColor: "white",
+        textAlign: "center",
+        alignItems: "center",
+        border: "1px solid gray",
+        cursor: "pointer",
+        padding: "0",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{ verticalAlign: "middle" }}
+      >
+        <path d="M9 18l6-6-6-6" />
+      </svg>
+    </button>
+  </div>
+</div>
+
+        <div className="carousel-container">
+          <Slider {...settings} ref={sliderRef}>
+            {visibleMovies.map((movie, index) => (
+              <MovieCard movie={movie} key={index} genres={genres} voteAverage={movie.vote_average} />
+            ))}
+          </Slider>
+        </div>
       </div>
-      <button className="showButton" onClick={toggleShowAll}>
-        {showAll ? "Hide Movies" : "Show More Movies"}
-      </button>
     </div>
-  );
+  );  
 };
 
 export default Home;
-
-
-
-
-
-
--- src/widgets/MovieCard/MovieCard.js
+++ src/widgets/MovieCard/MovieCard.js
@@ -4,7 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, dividerClasses } from '@mui/material';
 
 const MovieCard = ({ movie, genres, voteAverage }) => {
   const { poster_path, title, genre_ids, release_date, vote_average } = movie;
@@ -13,6 +13,7 @@ const MovieCard = ({ movie, genres, voteAverage }) => {
     margin: '10px',
     boxShadow: 'none',
     transition: 'color 0.7s',
+    borderRadius: 0,
   };
 
   const cardHoverStyle = {
@@ -29,37 +30,42 @@ const MovieCard = ({ movie, genres, voteAverage }) => {
   };
 
   return (
-    <Card sx={{ ...cardStyle, ...cardHoverStyle }}>
-      <CardActionArea>
-        <CardMedia sx={{ marginBottom: "10px" }}
-          component="img"
-          height="200"
-          image={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt={title}
-        />
-        <CardContent sx={{ padding: "0px" }}>
-          <Typography className='HoverTypography' variant="h6" sx={{ fontSize: '10px', marginBottom: "5px", width: "150px", textAlign: 'left', }} color="text.primary">
-            <strong>{title}</strong>
-          </Typography>
-          <Typography variant="body2" sx={{ fontSize: '10px', textAlign: 'left', }} color="text.secondary">
-          {release_date.slice(0, 4)}, {genres[genre_ids[0]]}
-          </Typography>
-          <Typography sx={{ ...ratingContainerStyle }}>
-            <Rating name="movie-rating" sx={{ textAlign: 'left', }} value={voteAverage / 2} readOnly />
-            <style>
-              {`
-                .MuiSvgIcon-root {
-                  width: 17px;
-                }
-              `}
-            </style>
-          </Typography>
-          <Typography variant="h6" sx={{ fontSize: '10px', marginBottom: "5px", width: "150px", textAlign: 'left', fontWeight: 700, color: "Gray" }}>
-            FOR FREE
-            </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+        <Card sx={{ ...cardStyle, ...cardHoverStyle }}>
+          <CardActionArea>
+            <CardMedia sx={{ marginBottom: "10px" }}
+              component="img"
+              height="200"
+              image={`https://image.tmdb.org/t/p/w500${poster_path}`}
+              alt={title}
+            />
+            <CardContent sx={{ padding: "0px" }}>
+              <Typography className='HoverTypography' variant="h6" sx={{ fontSize: '10px', marginBottom: "5px", width: "150px", textAlign: 'left', }} color="text.primary">
+                <strong>{title}</strong>
+              </Typography>
+              <Typography variant="body2" sx={{ fontSize: '10px', textAlign: 'left', }} color="text.secondary">
+              {release_date.slice(0, 4)}, {genres[genre_ids[0]]}
+              </Typography>
+              <Typography sx={{ ...ratingContainerStyle }}>
+                <Rating name="movie-rating" sx={{ textAlign: 'left', }} value={voteAverage / 2} readOnly />
+                {/* стили для звездочек рейтинга */}
+                <style> 
+                  {`
+                    .MuiPaper-root {
+                      margin-left: 0;
+                    }
+
+                    .MuiSvgIcon-root { 
+                      width: 17px;
+                    }
+                  `}
+                </style>
+              </Typography>
+              <Typography variant="h6" sx={{ fontSize: '10px', marginBottom: "5px", width: "150px", textAlign: 'left', fontWeight: 700, color: "Gray" }}>
+                FOR FREE
+                </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
   );
 }
 
